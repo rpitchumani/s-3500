48a00feb584442f53d6bccc2efb88227240853d3
rpitchumani <rpitchumani@gmail.com>
2022-05-26 13:06:16 -0400

    added new function get_particle_size_distribution_from_file_list

--- s3500_utils.py
+++ s3500_utils.py
@@ -10,10 +10,58 @@ import datetime
 from tqdm import tqdm
 import re
 
-from microtrac_s3500 import MicrotracS3500
+from s3500 import S3500
 
 
 def get_percentiles_from_file_list(psd_files: List) -> List[Dict]:
+
+    re_pattern_sample_id = "([LR]\d{8}-\d{3})_(\d*)_(\d*)_(\d*)_(\d*)_(\d*)_(\d*)"
+    list_d_values = []
+
+    for psd_file in tqdm(psd_files):
+
+        psd_pathlib = Path(psd_file)
+        psd_stem = psd_pathlib.stem
+
+        re_pattern_extract = re.findall(re_pattern_sample_id, psd_stem)
+
+        file_name_id = re_pattern_extract[0][0]
+        file_name_date = int(re_pattern_extract[0][1])
+        file_name_month = int(re_pattern_extract[0][2])
+        file_name_year = int(re_pattern_extract[0][3])
+        file_name_hour = int(re_pattern_extract[0][4])
+        file_name_minute = int(re_pattern_extract[0][5])
+        file_name_second = int(re_pattern_extract[0][6])
+
+        file_name_datetime = datetime.datetime(file_name_year, file_name_month, file_name_date, file_name_hour, file_name_minute, file_name_second)
+
+        try:
+            s3500 = S3500((psd_file))
+
+            # print(s3500.percentiles)
+            # print(file_name_id)
+
+            dict_psd = {
+                "file_name": psd_stem,
+                "file_name_id": file_name_id,
+                "file_name_date_time": file_name_datetime
+            }
+
+            for dp in s3500.percentiles["%Tile"].to_list():
+
+                dict_psd.update(
+                    {f"d{dp:.0f}": s3500.percentiles.loc[s3500.percentiles["%Tile"]==dp, "Size(um)"].iloc[0]}
+                )
+
+            list_d_values.append(dict_psd)
+
+        except Exception as e:
+            ...
+
+    return list_d_values
+
+
+def get_particle_size_distribution_from_file_list(psd_files: List) -> List[Dict]:
 
     re_pattern_sample_id = "([LR]\d{8}-\d{3})_(\d*)_(\d*)_(\d*)_(\d*)_(\d*)_(\d*)"
     list_psd = []
@@ -36,7 +84,7 @@ def get_percentiles_from_file_list(psd_files: List) -> List[Dict]:
         file_name_datetime = datetime.datetime(file_name_year, file_name_month, file_name_date, file_name_hour, file_name_minute, file_name_second)
 
         try:
-            s3500 = MicrotracS3500((psd_file))
+            s3500 = S3500((psd_file))
 
             # print(s3500.percentiles)
             # print(file_name_id)
@@ -44,14 +92,10 @@ def get_percentiles_from_file_list(psd_files: List) -> List[Dict]:
             dict_psd = {
                 "file_name": psd_stem,
                 "file_name_id": file_name_id,
-                "file_name_date_time": file_name_datetime
+                "file_name_date_time": file_name_datetime,
+                "psd": s3500.psd
             }
 
-            for dp in s3500.percentiles["%Tile"].to_list():
-
-                dict_psd.update(
-                    {f"d{dp:.0f}": s3500.percentiles.loc[s3500.percentiles["%Tile"]==dp, "Size(um)"].iloc[0]}
-                )
 
             list_psd.append(dict_psd)
 
